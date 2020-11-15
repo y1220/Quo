75b28487b5d3f7aed62e1664b89138d973aa758c
y1220 <ga.2.y1220@gmail.com>
2020-11-15 14:10:21 +0100

    added confirmation function

---
+++ config/environment.rb
@@ -0,0 +1,17 @@
+# Load the Rails application.
+require_relative 'application'
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'apikey',
+  :password => ENV["SENDGRID_PASSWORD"],
+  :domain => "smtp.gmail.com",
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
+	
