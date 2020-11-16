db7e57aeb06ba9dc2d894b2f8082174542efb440
y1220 <ga.2.y1220@gmail.com>
2020-11-16 23:00:23 +0100

    commented unlock token

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_11_14_170226) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index "\"unlock_token\"", name: "index_users_on_unlock_token", unique: true
+    #t.index "\"unlock_token\"", name: "index_users_on_unlock_token", unique: true
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
