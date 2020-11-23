f2e7e078c56ea84830705f51900545ff18425b95
y1220 <ga.2.y1220@gmail.com>
2020-11-23 19:24:55 +0100

    deleted the part in environment

--- config/environment.rb
+++ config/environment.rb
@@ -15,13 +15,13 @@ Rails.application.initialize!
 #}
 
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV["SENDGRID_USERNAME"],#'apikey',
-  :password => ENV["SENDGRID_PASSWORD"],
-  :domain =>  "quo2020.herokuapp.com",# "quo2020.herokuapp.com",
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+#ActionMailer::Base.smtp_settings = {
+#  :user_name => ENV["SENDGRID_USERNAME"],#'apikey',
+#  :password => ENV["SENDGRID_PASSWORD"],
+#  :domain =>  "quo2020.herokuapp.com",# "quo2020.herokuapp.com",
+#  :address => 'smtp.sendgrid.net',
+#  :port => 587,
+#  :authentication => :plain,
+#  :enable_starttls_auto => true
+#}
 	
