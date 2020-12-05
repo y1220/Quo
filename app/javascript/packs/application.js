937158706938b9f600013b5077c7b081685a853d
y1220 <ga.2.y1220@gmail.com>
2020-12-05 09:47:05 +0100

    utilised bootstrap, modified about page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,14 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
+
+
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+require("stylesheets/application.scss")
+
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
